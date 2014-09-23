cf29551ba924ad97546d7419b454740706e7814d
kondog <stepper97531@gmail.com>
2014-09-23 17:55:19 +0900

    first commit.

---
+++ nav_parser.rb
@@ -0,0 +1,94 @@
+#encoding: UTF-8
+require "strscan"
+
+class NavParser
+  attr_accessor :fields
+  def initialize
+    @fields = []
+  end
+  def parse_id( str )
+    str.delete(" ").delete(";")
+  end
+  def parse_name( str )
+    scanner = StringScanner.new( str )
+    name = ""
+    while not scanner.eos?
+      tmp_str = scanner.scan_until(/\w+/)
+      if not tmp_str.nil?
+        name += tmp_str unless tmp_str.include?( ';' )
+      else
+        return name
+      end
+    end
+  end
+  def parse_type( str )
+    scanner = StringScanner.new( str )
+    return scanner.scan(/\w+/)
+  end
+  def parse_caption( str )
+    scanner = StringScanner.new( str )
+    scanner.scan(/\s+/)
+    str_include_trash = scanner.scan_until(/[;}]/)
+    while str_include_trash[-1] =~ /[\s;}]/
+      str_include_trash.slice!(-1)
+    end
+    str_include_trash
+  end
+
+  def parse_fields( fields_str )
+    scanner = StringScanner.new( fields_str )
+    while !scanner.eos?
+      regexp = '\{.*?\}'
+      if scanner.check_until(/#{regexp}/m) != nil
+        # ?で最小マッチ
+        parse_each_fields( scanner.scan_until(/#{regexp}/m))
+      else
+        break
+      end
+    end
+  end
+
+  def parse_each_fields( field_str )
+    scanner = StringScanner.new( field_str )
+    scanner.scan_until( /{/ )
+    id     = parse_id( scanner.scan_until( /;/ ) )
+    enable = scanner.scan_until( /;/ )
+    name   = parse_name(scanner.scan_until( /;/ ))
+    type   = parse_type(scanner.scan_until( /;/ ))
+    caption = nil
+    while !scanner.eos?
+      str_until_semicolon = scanner.scan_until( /[;}]/ )
+      case 
+      when str_until_semicolon.nil?
+        break
+      when str_until_semicolon.include?( "CaptionML" )
+        caption = parse_caption(str_until_semicolon)
+      end
+    end
+    @fields << {:ID => id, :Enable => "", :Name => name, :Type => type, :Caption => caption }
+  end
+
+  def parse_file_data( file_str )
+    scanner = StringScanner.new( file_str )
+    scanner.scan_until(/FIELDS/)
+    scanner.scan(/\s+{\s*/)
+    scanner.scan_until(/KEYS/)
+  end
+end
+
+if $0 == __FILE__
+  if ARGV[0].nil?
+    puts 'Input File Name in 1st argument.'
+    exit(1)
+  elsif !File.exists? ARGV[0]
+    puts "File not exists: #{ARGV[0]}"
+    exit(1)
+  end
+  nav_parser = NavParser.new
+  file_data = IO.read(ARGV[0]).encode("UTF-16BE", "UTF-8",
+                                      :invalid => :replace,
+                                      :undef   => :replace,
+                                      :replace => '?').encode("UTF-8")
+  file_data_parsed = nav_parser.parse_file_data( file_data )
+  nav_parser.parse_fields( file_data_parsed )
+end
---
+++ test_nav_parser.rb
@@ -0,0 +1,135 @@
+require "test/unit"
+require "./nav_parser.rb"
+require "strscan"
+
+class TC_NAV_P < Test::Unit::TestCase
+  def setup
+    @nav = NavParser.new
+  end
+
+  def test_parse_id
+    assert_equal(  '1', @nav.parse_id( ' 1   ;' ))
+  end
+
+  def test_parse_name
+    assert_equal(  'Package Code', @nav.parse_name( 'Package Code        ;') )
+  end
+
+  def test_parse_type
+    assert_equal('Code20', @nav.parse_type( 'Code20        ;'))
+  end
+
+  def test_parse_caption
+    assert_equal( 'CaptionML=ENU=Package Code', @nav.parse_caption(<<'EOS'))
+                                               CaptionML=ENU=Package Code }
+EOS
+  end
+
+  def test_each_fields_parse
+    @nav.parse_each_fields(<<'EOS')
+{ 1   ;   ;Package Code        ;Code20        ;TableRelation="Config. Package";
+                                               CaptionML=ENU=Package Code }
+EOS
+    assert_equal( 
+                 {:ID      => '1',
+                  :Enable  => '',
+                  :Name    => 'Package Code',
+                  :Type    => 'Code20', 
+                  :Caption => 'CaptionML=ENU=Package Code'
+                 }, 
+                @nav.fields[0] 
+                )
+  end
+
+  def test_fields_parse
+    @nav.parse_fields(<<'EOS')
+    { 1   ;   ;Package Code        ;Code20        ;TableRelation="Config. Package";
+                                                   CaptionML=ENU=Package Code }
+    { 2   ;   ;Table ID            ;Integer       ;TableRelation=Object.ID WHERE (Type=CONST(Table));
+                                                   OnValidate=BEGIN
+                                                                IF ConfigMgt.IsSystemTable("Table ID") THEN
+                                                                  ERROR(Text001,"Table ID");
+
+                                                                IF "Table ID" <> xRec."Table ID" THEN
+                                                                  "Page ID" := ConfigMgt.FindPage("Table ID");
+                                                              END;
+
+                                                   OnLookup=BEGIN
+                                                              ConfigValidateMgt.LookupTable("Table ID");
+                                                              IF "Table ID" <> 0 THEN
+                                                                VALIDATE("Table ID");
+                                                            END;
+
+                                                   CaptionML=ENU=Table ID;
+                                                   NotBlank=Yes }
+    { 3   ;   ;Table Name          ;Text250       ;FieldClass=FlowField;
+                                                   CalcFormula=Lookup(AllObjWithCaption."Object Name" WHERE (Object Type=CONST(Table),
+                                                                                                             Object ID=FIELD(Table ID)));
+                                                   CaptionML=ENU=Table Name;
+                                                   Editable=No }
+EOS
+    assert_equal( 
+                 {:ID      => '1',
+                  :Enable  => '',
+                  :Name    => 'Package Code',
+                  :Type    => 'Code20', 
+                  :Caption => 'CaptionML=ENU=Package Code'
+                 }, 
+                @nav.fields[0] 
+                )
+    assert_equal( 
+                 {:ID      => '2',
+                  :Enable  => '',
+                  :Name    => 'Table ID',
+                  :Type    => 'Integer', 
+                  :Caption => 'CaptionML=ENU=Table ID'
+                 }, 
+                @nav.fields[1] 
+                )
+    assert_equal( 
+                 {:ID      => '3',
+                  :Enable  => '',
+                  :Name    => 'Table Name',
+                  :Type    => 'Text250', 
+                  :Caption => 'CaptionML=ENU=Table Name'
+                 }, 
+                @nav.fields[2] 
+                )
+  end
+
+  def test_parse_file_data
+    assert_equal( <<'INPUT1'[0..-2], @nav.parse_file_data(<<'EOS')
+{ 1   ;   ;Package Code        ;Code20        ;TableRelation="Config. Package";
+                                                   CaptionML=ENU=Package Code }
+  }
+  KEYS
+INPUT1
+OBJECT Table 8613 Config. Package Table
+{
+  OBJECT-PROPERTIES
+  {
+    Version List=NAVW17.00,S-003;
+  }
+  PROPERTIES
+  {
+    CaptionML=ENU=Config. Package Table;
+  }
+  FIELDS
+  {
+    { 1   ;   ;Package Code        ;Code20        ;TableRelation="Config. Package";
+                                                   CaptionML=ENU=Package Code }
+  }
+  KEYS
+  {
+  }
+  FIELDGROUPS
+  {
+  }
+  CODE
+  {
+  }
+}
+EOS
+                )
+  end
+end
